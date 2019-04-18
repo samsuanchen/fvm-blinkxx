888ea69e2014685f260df053b37801cadf75fcb5
samsuanchen <samsuanchen@gmail.com>
2019-04-18 15:26:09 +0800

    update

--- blink00/blink00.ino
+++ blink00/blink00.ino
@@ -1,13 +1,11 @@
-  // blink00.ino the Arduino basic blink
-  #define LED_BUILTIN 16            // for WIFIBOY
-  
-  
-  void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);   // set led pin as output
-  }
-  void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);// set led pin level as HIGH
-    delay(1000);                    // wait a second
-    digitalWrite(LED_BUILTIN, LOW); // set led pin level as LOW
-    delay(1000);                    // wait a second
-  }
+// blink00.ino the Arduino basic blink
+#define LED_BUILTIN 16            // for WIFIBOY
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);   // set led pin as output
+}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);// set led pin level as HIGH
+  delay(1000);                    // wait a second
+  digitalWrite(LED_BUILTIN, LOW); // set led pin level as LOW
+  delay(1000);                    // wait a second
+}
